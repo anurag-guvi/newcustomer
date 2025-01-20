4722b45b4d56a5e7bc94b07fac92c6ecac5bd4ea
anurag-guvi <anuragpatil05524@gmail.com>
2025-01-20 14:58:53 +0530

    made changes to github secrets and test file

--- src/test/java/com/example/newcustomer/NewcustomerApplicationTests.java
+++ src/test/java/com/example/newcustomer/NewcustomerApplicationTests.java
@@ -2,12 +2,13 @@ package com.example.newcustomer;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class NewcustomerApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
-
 }
